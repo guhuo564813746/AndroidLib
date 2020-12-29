49c5228c47a9ff56f305e22792a8ff3fb21537a1
zhangjin <Zhangjin@seafarer.me>
2020-12-29 18:09:38 +0800

    方法库更新

---
+++ AndroidCodeLib/src/main/java/com/zj/androidcodelib/utils/FileUtils.java
@@ -0,0 +1,8 @@
+package com.zj.androidcodelib.utils;
+
+/**
+ * 作者：create by 123456 on 2020/12/29 15:16
+ * 邮箱：564813746@qq.com
+ */
+public class FileUtils {
+}
---
+++ AndroidCodeLib/src/main/java/com/zj/androidcodelib/utils/SysUtils.java
@@ -0,0 +1,9 @@
+package com.zj.androidcodelib.utils;
+
+/**
+ * 作者：create by 123456 on 2020/12/29 15:17
+ * 邮箱：564813746@qq.com
+ */
+public class SysUtils {
+
+}
