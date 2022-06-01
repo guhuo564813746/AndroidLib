8a0874c490794ca90532c3b7e543ef02c617537b
zhangjin <zhangjin@seafarer.me>
2022-06-01 18:55:40 +0800

    方法库更新

---
+++ app/src/main/java/com/lib/audio/activitis/AudioRecordTestActivity.java
@@ -0,0 +1,20 @@
+package com.lib.audio.activitis;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * 作者：create by 张金 on 2022/6/1 10:20
+ * 邮箱：564813746@qq.com
+ */
+public class AudioRecordTestActivity extends AppCompatActivity {
+    private static final String TAG="AudioRecordTestActivity";
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate--");
+    }
+}
